bfbe9c48389255aedcac87871cdfba858d469338
LesterGarcia34 <lagarciaaq@correo.url.edu.gt>
2017-11-18 09:37:53 -0600

    Add files via upload

---
+++ TraductorEstructuras/src/traductorestructuras/TraductorEstructuras.java
@@ -0,0 +1,11 @@
+package traductorestructuras;
+
+public class TraductorEstructuras {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
